3e81cfddcec2c7ced6dbc8eec364150fdc4674ea
Nik <nikyak20062000@gmail.com>
2024-11-29 05:17:18 +0300

    PZ_5_2.py

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -4,7 +4,7 @@ C — вещественные параметры, являющиеся одно
 выходными). С помощью этой функции упорядочить по убыванию два данных
 набора из трех чисел: (A1, B1, C1) и (A2, B2, C2)."""
 
-
+input
 def SortDec(A, B, C):
     if A >= B and A >= C:
         max_num = A
@@ -16,32 +16,25 @@ def SortDec(A, B, C):
             min_num = B
     elif B >= A and B >= C:
         max_num = B
-        if num1 >= num3:
-            mid_num = num1
-            min_num = num3
+        if B >= C:
+            mid_num = A
+            min_num = C
         else:
-            mid_num = num3
-            min_num = num1
+            mid_num = C
+            min_num = A
     else:
-        max_num = num3
-        if num1 >= num2:
-            mid_num = num1
-            min_num = num2
+        max_num = C
+        if A >= B:
+            mid_num = A
+            min_num = B
         else:
-            mid_num = num2
-            min_num = num1
+            mid_num = B
+            min_num = A
 
     return max_num, mid_num, min_num
 
-
-# Пример наборов чисел
-first_number1, first_number2, first_number3 = 5.2, 3.8, 4.1
-second_number1, second_number2, second_number3 = 2.5, 3.6, 1.9
-
 # Сортируем первый набор чисел
-first_number1, first_number2, first_number3 = sort_numbers_descending(first_number1, first_number2, first_number3)
-print(
-    f"Первый набор отсортирован: first_number1 = {first_number1}, first_number2 = {first_number2}, first_number3 = {first_number3}")
+print("Первый набор отсортирован:", first_number1 = first_number1, first_number2 = first_number2, first_number3 = first_number3")
 
 # Сортируем второй набор чисел
 second_number1, second_number2, second_number3 = sort_numbers_descending(second_number1, second_number2, second_number3)
