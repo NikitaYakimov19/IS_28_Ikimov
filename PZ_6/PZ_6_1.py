4ffd6878e12fdba4463925649476054efeaf9cf0
Никита <nikyak20062000@gmail.com>
2024-12-07 10:50:00 +0300

    PZ_6 Ура, сделано

--- PZ_6/PZ_6_1.py
+++ PZ_6/PZ_6_1.py
@@ -1,25 +1,35 @@
 """ Дан список размера N. Поменять местами его минимальный и максимальный
 элементы. """
 
-user_input = input("Введите список чисел, разделенных запятыми: ")
-user_input = user_input.split(',')
-user_input = [int(num) for num in user_input]  # Преобразуем каждое число в целое
+try:
+    user_input = input("Введите список чисел, разделенных запятыми: ")
 
-# Находим индекс максимального числа
-max_index = user_input.index(max(user_input))
+    # Проверка на наличие запятых
+    if ',' not in user_input:
+        print("Ошибка: Введите числа, разделенные запятыми.")
+    else:
+        user_input = user_input.split(',')
+        user_input = [int(num.strip()) for num in user_input]  # Преобразуем каждое число в целое
 
-# Находим индекс минимального числа
-min_index = user_input.index(min(user_input))
+        # Находим индекс максимального числа
+        max_index = user_input.index(max(user_input))
 
-# Сохраняем значения максимального и минимального чисел
-max_value = user_input[max_index]
-min_value = user_input[min_index]
+        # Находим индекс минимального числа
+        min_index = user_input.index(min(user_input))
 
-# Меняем местами максимальное и минимальное числа
-user_input[max_index] = min_value
-user_input[min_index] = max_value
+        # Сохраняем значения максимального и минимального чисел
+        max_value = user_input[max_index]
+        min_value = user_input[min_index]
 
-# Выводим измененный список как строку без скобок и запятых
-result = ', '.join(map(str, user_input))
-print(result)
+        # Меняем местами максимальное и минимальное числа
+        user_input[max_index] = min_value
+        user_input[min_index] = max_value
 
+        # Выводим измененный список как строку без скобок и запятой
+        result = ', '.join(map(str, user_input))
+        print("Поменяв местами максимальный и минимальный элемент списка получилось: ", result)
+
+except ValueError:
+    print("Ошибка: Убедитесь, что вы ввели корректные целые числа.")
+except Exception as e:
+    print("Произошла ошибка:", e)
