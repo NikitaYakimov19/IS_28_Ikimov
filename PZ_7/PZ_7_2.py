b62f79417e712a68e49e912627f057ccd9716588
NikitaYakimov19 <nikyak20062000@gmail.com>
2025-01-22 04:30:28 +0300

    PZ_7 и PZ_9 сделаны

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -2,12 +2,14 @@
 расположенные на четных позициях строки, а затем, в обратном порядке, все
 символы, расположенные на нечетных позициях (например, строка «Программа»
 превратится в «ргамамроП»)."""
-sent = list(input("Введите строку/предложение: "))
+sent = input("Введите строку/предложение: ")
+print("Исходная строка/предложение ", sent)
+sent2 = list(sent)
 even_char = ""
 odd_char = ""
-for index in range(len(sent)):
+for index in range(len(sent2)):
     if index % 2 == 0:
-        even_char += sent[index]
+        even_char += sent2[index]
     else:
-        odd_char += sent[index]
+        odd_char += sent2[index]
 print(odd_char + even_char[::-1])
